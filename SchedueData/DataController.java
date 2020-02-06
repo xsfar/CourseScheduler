2822dd31451e9a46fe04173ff8796c77c91dcd7f
Ilyass Sfar <42755577+cloud0x9@users.noreply.github.com>
2020-02-05 19:46:49 -0500

    Add files via upload

---
+++ SchedueData/DataController.java
@@ -0,0 +1,11 @@
+package SchedueData;
+
+import java.io.IOException;
+
+public class DataController {
+
+ public static void main(String[] args) throws IOException {
+     
+SchedueData.ParseData.String();
+ 
+ }}
