8003668b644aa3bb48b94eacf4319e879cd02a22
Nick Econopouly <wry@mm.st>
2020-02-09 00:34:14 -0500

    Add pom.xml with Selenium dependencies.
    This commit also refactors the existing 4 java files into a proper
    Maven project structure (without changing the logic).

---
+++ src/main/java/ScheduleCreator/DataController.java
@@ -0,0 +1,12 @@
+package ScheduleCreator;
+//run this
+import java.io.IOException;
+//not really a Controller yet, but just a holder for now 
+public class DataController {
+
+ public static void main(String[] args) throws IOException {
+
+ //
+ScheduleCreator.ParseData.String();
+ 
+ }}
