f38b12b0ffa6ce0d24789795f66c0be4129ac603
Jamison Valentine <jamisont.valentine@gmail.com>
2020-02-20 21:21:39 -0500

    rename fxml files, move primary Controller to new controllers package, add functionality to SelectCourses view

---
+++ src/ScheduleCreator/controllers/CoursesController.java
@@ -0,0 +1,59 @@
+package ScheduleCreator.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author jamis
+ */
+public class CoursesController {
+    
+    private int rowIndex = 0;
+    private int colIndex = 0;
+    private int numberOfCourses = 0;
+    
+    @FXML private GridPane courses;
+    @FXML private Button first;
+    
+    
+        public void backToPrimary(ActionEvent _event) throws Exception {
+            
+            Parent root = FXMLLoader.load(getClass().getResource("/ScheduleCreator/resources/primary.fxml"));
+            Scene primaryScene = new Scene(root);
+            
+            //Get information from primary stage
+            Stage window = (Stage)((Node)_event.getSource()).getScene().getWindow();
+            window.setScene(primaryScene);
+            window.show();
+        }
+        
+        @FXML
+        public void addCourse(ActionEvent _event) {
+            
+            //Go to next row after every 3rd course.
+            rowIndex = numberOfCourses / 3;
+            
+            //Go back to first column after every 3rd course.
+            colIndex = numberOfCourses % 3;
+            
+            HBox newBox = new HBox(new Label("Course 1"));
+            newBox.setMinHeight(100);
+            newBox.setMinWidth(100);
+            newBox.setStyle("-fx-border-color: black");
+            courses.getChildren().add(newBox);
+            GridPane.setRowIndex(newBox, rowIndex);
+            GridPane.setColumnIndex(newBox, colIndex);
+            numberOfCourses++;
+        }
+}
--- src/ScheduleCreator/controllers/PrimaryController.java
+++ src/ScheduleCreator/controllers/PrimaryController.java
@@ -1,13 +1,14 @@
-package ScheduleCreator;
+package ScheduleCreator.controllers;
 
 /**
  * This class serves as the controller for the primary view
  * 
  * @author Jamison Valentine, Ilyass Sfar, Nick Econopouly, Nathan Tolodzieki
  * 
- * Last Updated: 2/17/2020
+ * Last Updated: 2/20/2020
  */
 
+import ScheduleCreator.*;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -28,7 +29,7 @@ public class PrimaryController implements Initializable {
         public void changeToSelectClasses(ActionEvent _event) throws Exception {
             
             //new FXML loader and scene for new screen
-            Parent root = FXMLLoader.load(getClass().getResource("resources/select_classes.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/ScheduleCreator/resources/select_courses.fxml"));
             Scene classViewScene = new Scene(root);
             
             //Get information from primary stage
@@ -37,20 +38,9 @@ public class PrimaryController implements Initializable {
             window.show();
         }
         
-        public void backToPrimary(ActionEvent _event) throws Exception {
-            
-            Parent root = FXMLLoader.load(getClass().getResource("resources/primary.fxml"));
-            Scene primaryScene = new Scene(root);
-            
-            //Get information from primary stage
-            Stage window = (Stage)((Node)_event.getSource()).getScene().getWindow();
-            window.setScene(primaryScene);
-            window.show();
-        }
-        
         public void changeToRegistrationScreen(ActionEvent _event) throws Exception {
             
-            Parent root = FXMLLoader.load(getClass().getResource("resources/registration_screen.fxml"));
+            Parent root = FXMLLoader.load(getClass().getResource("/ScheduleCreator/resources/registration_screen.fxml"));
             Scene scene = new Scene(root);
             
             Stage stage = (Stage)((Node)_event.getSource()).getScene().getWindow();
