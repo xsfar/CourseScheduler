4608be99ac64f876168d7884e04f0397f77bb44b
Nathan M Tolodziecki <NMTOLODZ@uncg.edu>
2020-02-21 17:37:55 -0500

    add some methods for Section and Schedule

--- ScheduleCreator/models/Schedule.java
+++ ScheduleCreator/models/Schedule.java
@@ -1,5 +1,7 @@
 package ScheduleCreator.models;
 
+import java.util.LinkedList;
+
 /**
  * This class keeps track of added course sections and produces CRNs.
  *
@@ -8,12 +10,9 @@ package ScheduleCreator.models;
 
 public class Schedule {
 
-    protected Section[] sections;
+    protected LinkedList<Section> sections;
     protected int credits;
     //checkConflicts
-    //addSection
-    //removeSection
-    //getCRNS
     //saveSchedule
     //resetSchedule
     //generateRandomSchedules
@@ -22,4 +21,34 @@ public class Schedule {
 
     }
 
+    public Schedule(LinkedList<Section> _sections) {
+        this.sections = _sections;
+        this.sections.forEach((section) -> {
+            this.credits += section.getCreditHours();
+        });
+    }
+    
+    public void addSection(Section _section){
+        this.sections.add(_section);
+    }
+    
+    public boolean removeSection(Section _section){
+        return this.sections.remove(_section);
+    }
+    
+    //============================ GETTERS ============================
+
+    public LinkedList<String> getCRNS() {
+        LinkedList listOfCRNS = new LinkedList<>();
+        
+        this.sections.forEach((section) -> {
+            listOfCRNS.add(section.getCRN());
+        });
+        
+        return listOfCRNS;
+    }
+    
+    public int getCredits(){
+        return this.credits;
+    }
 }
--- ScheduleCreator/models/Section.java
+++ ScheduleCreator/models/Section.java
@@ -1,51 +1,71 @@
 package ScheduleCreator.models;
 
 import java.util.HashMap;
+import java.util.Objects;
 
 /**
  * This class models information for course sections.
  *
  * @author Jamison Valentine, Ilyass Sfar, Nick Econopouly, Nathan Tolodzieki
  *
- * Last Updated: 2/17/2020
+ * Last Updated: 2/21/2020
  */
-
 public class Section {
 
     protected String location;
     protected String instructor;
     protected HashMap daysAndTimes;
-    protected String CRN;
+    protected int CRN;
     protected int sectionNumber;
+    protected int creditHours;
 
     public Section() {
 
     }
 
-//=================  GETTERS ===============
-
-    public HashMap getDaysAndTimes() {
-        return daysAndTimes;
+    public Section(String _location, String _instructor, HashMap _daysAndTimes, int _CRN, int _sectionNumber, int _creditHours) {
+        this.location = _location;
+        this.instructor = _instructor;
+        this.daysAndTimes = _daysAndTimes;
+        this.CRN = _CRN;
+        this.sectionNumber = _sectionNumber;
+        this.creditHours = _creditHours;
     }
 
-    public String getCRN() {
-        return CRN;
+    @Override
+    public boolean equals(Object _section) {
+        if (_section instanceof Section) {
+            return ((Section) _section).CRN == this.CRN;
+        }
+        return false;
+    }
+
+//=================  GETTERS ===============
+    public HashMap getDaysAndTimes() {
+        return this.daysAndTimes;
+    }
+
+    public int getCRN() {
+        return this.CRN;
     }
 
     public String getInstructor() {
-        return instructor;
+        return this.instructor;
     }
 
     public String getLocation() {
-        return location;
+        return this.location;
     }
 
     public int getSectionNumber() {
-        return sectionNumber;
+        return this.sectionNumber;
+    }
+
+    public int getCreditHours() {
+        return this.creditHours;
     }
 
 //=================  SETTERS ===============
-
     public void setSectionNumber(int _sectionNumber) {
         this.sectionNumber = _sectionNumber;
     }
@@ -62,7 +82,11 @@ public class Section {
         this.daysAndTimes = _daysAndTimes;
     }
 
-    public void setCRN(String _CRN) {
+    public void setCRN(int _CRN) {
         this.CRN = _CRN;
+    }
+
+    public void setCreditHours(int _creditHours) {
+        this.creditHours = _creditHours;
     }
 }
