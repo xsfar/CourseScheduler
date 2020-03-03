224ac361135e8f8443d340c88defbf5661e2ba4d
Sfar <isfar341@gmail.com>
2020-03-03 11:51:03 -0500

    Formatting, and added new text file

--- ScheduleCreator/DBAdapter.java
+++ ScheduleCreator/DBAdapter.java
@@ -122,25 +122,27 @@ public class DBAdapter {
         input.close();
         writer.close();
     }
+
     //given a class name (ex CSC 230) the times for all sections are resturned.
     //given a class name with section (ex. CSC 230 - 01) only the time for the class is returned
     public static void getTime(String _abbreviation) throws Exception {
         //regex expression to get time from the current format
-      final String time = "(?<=([ ]\\b[A-Z]{3}\\b.\\b[0-9]{3}\\b.+ [0-9]{2}\\b)).+?(?=\\b([	](TR\\b|MW\\b|MWF\\b|M\\b|T\\b|W\\b|R\\b|F\\b|(TBA.*TBA\\b)))\\b)";
+        final String time = "(?<=([ ]\\b[A-Z]{3}\\b.\\b[0-9]{3}\\b.+ [0-9]{2}\\b)).+?(?=\\b([	](TR\\b|MW\\b|MWF\\b|M\\b|T\\b|W\\b|R\\b|F\\b|(TBA.*TBA\\b)))\\b)";
         List<String> lines = Files.readAllLines(Paths.get("src/ScheduleCreator/resources/raw/CoursesTimeDate.txt"));
         for (String line : lines) {
             if (line.contains(_abbreviation)) {
                 String results = line;
-                
-                 Matcher match = Pattern.compile(time).matcher(results);
-                 while (match.find()) {
 
-                 String output = (match.group());
-                     System.out.println(output);
+                Matcher match = Pattern.compile(time).matcher(results);
+                while (match.find()) {
+
+                    String output = (match.group());
+                    System.out.println(output);
+
+                }
 
             }
-
         }
     }
-}
+
 }
