e55b5e4b10f13ea32e999df2d2e543fafd27a8e5
Ilyass Sfar <42755577+cloud0x9@users.noreply.github.com>
2020-04-13 11:12:29 -0400

    Added a Dark Mode option (#61)
    * Added a darkmode option
    
    * Update fall2020 file, since last time a lot of courses have added times and days previously just "TBA"

--- ScheduleCreator/controllers/PrimaryController.java
+++ ScheduleCreator/controllers/PrimaryController.java
@@ -7,7 +7,7 @@ package ScheduleCreator.controllers;
  *
  * Last Updated: 3/18/2020
  */
-
+import com.sun.javafx.css.StyleManager;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
@@ -16,9 +16,15 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ToggleButton;
 
 public class PrimaryController implements Initializable {
+
+    @FXML
+    private ToggleButton darkmode;
 
     public void changeToSelectClasses(ActionEvent _event) throws Exception {
 
@@ -41,6 +47,18 @@ public class PrimaryController implements Initializable {
         Stage stage = (Stage) ((Node) _event.getSource()).getScene().getWindow();
         stage.setScene(scene);
         stage.show();
+    }
+
+    //toggle and untoggle darkmode css to defult theme from button
+    @FXML
+    void toggleDarkMode(ActionEvent event) {
+        if (darkmode.isSelected()) {
+            Application.setUserAgentStylesheet(Application.STYLESHEET_MODENA);
+            StyleManager.getInstance().addUserAgentStylesheet("ScheduleCreator/resources/Darkmode.css");
+        } else {
+            Application.setUserAgentStylesheet(Application.STYLESHEET_MODENA);
+            StyleManager.getInstance().addUserAgentStylesheet(null);
+        }
     }
 
     @Override
